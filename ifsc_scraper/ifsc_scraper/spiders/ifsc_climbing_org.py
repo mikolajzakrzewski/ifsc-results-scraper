6b607e9068627579384c8708673a88c5010aedff
Mikołaj Zakrzewski <mikizakr@gmail.com>
2024-06-27 18:48:50 +0200

    Reaching athlete info pages; Saving Event and Category data to the database

--- ifsc_scraper/ifsc_scraper/spiders/ifsc_climbing_org.py
+++ ifsc_scraper/ifsc_scraper/spiders/ifsc_climbing_org.py
@@ -1,29 +1,14 @@
 import scrapy
-import requests
 import json
+import re
+from unidecode import unidecode
 from scrapy.spiders import Spider
 from ..items import EventItem, CategoryItem, AthleteItem, EntryItem
 
 # URLs of the website
 domain_name = "ifsc-climbing.org"
-base_url = f"https://{domain_name}"
+base_url = f"https://www.{domain_name}"
 api_url = f"{base_url}/api/dapi"
-
-# Passed parameters abbreviations and their full names
-league_names = {
-    "wc": "World Cups and World Championships",
-    "y": "IFSC Youth",
-    "pc": "IFSC Paraclimbing (L)",
-    "aa": "IFSC Asia Adults",
-    "ay": "IFSC Asia Youth",
-    "ea": "IFSC Europe Adults",
-    "ey": "IFSC Europe Youth",
-    "pa": "IFSC Panam",
-    "oc": "IFSC Oceania",
-    "g": "Games",
-    "oe": "Other events",
-    "m": "Masters and Promotional Events"
-}
 
 discipline_names = {
     "l": "lead",
@@ -50,10 +35,9 @@ class IfscClimbingOrgSpider(Spider):
     name = "ifsc_climbing_org"
     allowed_domains = [domain_name]
 
-    def __init__(self, years, leagues, disciplines, categories, *args, **kwargs):
+    def __init__(self, years, disciplines, categories, *args, **kwargs):
         super(IfscClimbingOrgSpider, self).__init__(*args, **kwargs)
         self.years = years.split(',')
-        self.leagues = [league_names[league] for league in leagues.split(',')]
         self.disciplines = [discipline_names[discipline] for discipline in disciplines.split(',')]
         self.categories = [category_names[category] for category in categories.split(',')]
         self.start_urls = [
@@ -71,34 +55,39 @@ class IfscClimbingOrgSpider(Spider):
 # Gather the event IDs for the given year, leagues and discipline kinds
     def parse_year(self, response):
         json_data = json.loads(response.text)
+        print(json_data)
         for event in json_data['items']:
             event_url = event['url']
             event_name = event['title']
             event_date = event['fields']['dateFrom'][0:10]
+            event_location = event['fields']['location']
             yield scrapy.Request(
                 f"{event_url}result/index",
                 callback=self.parse_event,
-                cb_kwargs={"date": event_date}
+                cb_kwargs={"event_name": event_name, "event_date": event_date, "event_location": event_location}
             )
 
 # Gather the full result URLs for the given event, discipline kinds and categories
-    def parse_event(self, response, date):
+    def parse_event(self, response, event_name, event_date, event_location):
         text_data = response.text
 
         # Extract the event page URL from the response
-        start_str = "pageName\\\",\\\""
-        end_str = "\\\","
-        start_index = text_data.find(start_str)
-        end_index = text_data.find(end_str, start_index + len(start_str))
-        event_page = text_data[start_index + len(start_str):end_index]
-        event_url = f"https://www.{domain_name}/{event_page}"
+        event_url = response.url
 
         # Extract the event ID from the response
         start_str = "eventId\\\":\\\""
         end_str = "\\\","
         start_index = text_data.find(start_str)
         end_index = text_data.find(end_str, start_index + len(start_str))
-        event_id = text_data[start_index + len(start_str):end_index]
+        # TODO: dont save events that dont fit the criteria
+        if start_index != -1 and end_index != -1:
+            event_id = text_data[start_index + len(start_str):end_index]
+            yield EventItem(
+                event_id=event_id,
+                name=event_name,
+                date=event_date,
+                location=event_location
+            )
 
         # Extract the discipline & category IDs from the response
         start_str = "navigationItems\\\":"
@@ -110,19 +99,55 @@ class IfscClimbingOrgSpider(Spider):
 
         for category_id, category_data in event_data.items():
             if category_data['discipline'] in self.disciplines and category_data['category'] in self.categories:
+                headers = {
+                    "Next-Action": "efa2fa106f24654dd09188f3c815302653521600"
+                }
                 payload = [{"event_id": event_id, "id": category_id}]
+                yield CategoryItem(
+                    event_id=event_id,
+                    category_id=category_id,
+                    discipline=category_data['discipline'],
+                    name=category_data['category']
+                )
+
                 yield scrapy.Request(
                     event_url,
                     callback=self.parse_results,
                     method="POST",
-                    headers={"Next-Action": "efa2fa106f24654dd09188f3c815302653521600"},
+                    headers=headers,
                     body=json.dumps(payload),
-                    cb_kwargs={"date": date, "category_id": category_id},
+                    cb_kwargs={"event_date": event_date, "category_id": category_id},
                 )
 
 # Gather data from the full results of the given event
-    def parse_results(self, response, date, category_id):
+    def parse_results(self, response, event_date, category_id):
+        response.encoding = 'utf-8'
         data = response.text
         separator_index = data.find("1:")
         data = data[separator_index + 2:].strip()
-        event_data = json.loads(data)["data"]
+        ranking = json.loads(data)["data"]["ranking"]
+        for athlete in ranking:
+            athlete_id = athlete["athlete_id"]
+            rank = athlete["rank"]
+
+            # Convert the athlete's name to a URL-friendly format
+            name = athlete["firstname"] + " " + athlete["lastname"].strip()
+            name = unidecode(name)
+            name = name.lower()
+            name = re.sub(r"[^a-z]", "-", name)
+
+            # Combine the athlete's name with the athlete's ID to form the athlete's URL
+            athlete_url = f"{base_url}/athlete/{athlete_id}/{name}"
+            # yield scrapy.Request(
+            #     athlete_url,
+            #     callback=self.parse_athlete,
+            #     cb_kwargs={"rank": rank, "event_date": event_date, "category_id": category_id}
+            # )
+
+    def parse_athlete(self, response, rank, event_date, category_id):
+        response.encoding = 'utf-8'
+        data = response.text
+        pass
+
+    def parse_entry(self, response, rank, event_date, category_id):
+        pass
