fa8fd5ed38aed6f3f733ff0817cf7cd79a267dee
Mikołaj Zakrzewski <mikizakr@gmail.com>
2024-06-28 14:21:15 +0200

    Saving athlete info to the database in the ifsc_climbing_org spider

--- ifsc_scraper/ifsc_scraper/spiders/ifsc_climbing_org.py
+++ ifsc_scraper/ifsc_scraper/spiders/ifsc_climbing_org.py
@@ -2,6 +2,7 @@ import scrapy
 import json
 import re
 from unidecode import unidecode
+from bs4 import BeautifulSoup
 from scrapy.spiders import Spider
 from ..items import EventItem, CategoryItem, AthleteItem, EntryItem
 
@@ -116,12 +117,12 @@ class IfscClimbingOrgSpider(Spider):
                     method="POST",
                     headers=headers,
                     body=json.dumps(payload),
-                    cb_kwargs={"event_date": event_date, "category_id": category_id},
+                    cb_kwargs={"category_id": category_id, "event_date": event_date},
                 )
 
 # Gather data from the full results of the given event
-    def parse_results(self, response, event_date, category_id):
-        response.encoding = 'utf-8'
+    def parse_results(self, response, category_id, event_date):
+        # response.encoding = 'utf-8'
         data = response.text
         separator_index = data.find("1:")
         data = data[separator_index + 2:].strip()
@@ -138,16 +139,60 @@ class IfscClimbingOrgSpider(Spider):
 
             # Combine the athlete's name with the athlete's ID to form the athlete's URL
             athlete_url = f"{base_url}/athlete/{athlete_id}/{name}"
-            # yield scrapy.Request(
-            #     athlete_url,
-            #     callback=self.parse_athlete,
-            #     cb_kwargs={"rank": rank, "event_date": event_date, "category_id": category_id}
-            # )
+            yield scrapy.Request(
+                athlete_url,
+                callback=self.parse_athlete,
+                cb_kwargs={"category_id": category_id, "rank": rank, "event_date": event_date}
+            )
 
-    def parse_athlete(self, response, rank, event_date, category_id):
-        response.encoding = 'utf-8'
-        data = response.text
-        pass
+    def parse_athlete(self, response, category_id, rank, event_date):
+        # Extract the part of the response that contains the athlete's information
+        soup = BeautifulSoup(response.text, 'html.parser')
+        script_tags = soup.find_all('script')
+        search_text = 'firstname'
+        script_text = [script.string for script in script_tags if script.string and search_text in script.string]
+        script_text = script_text[0]
+        script_text = script_text.replace("\\", "")
+        match = re.search(r'{"athlete":{.*', script_text)
+        if match:
+            script_text = match.group()
 
-    def parse_entry(self, response, rank, event_date, category_id):
+            # Remove trailing characters from the text to get the JSON data
+            opening_braces = script_text.count('{')
+            closing_braces = script_text.count('}')
+            closing_brace_index = closing_braces - opening_braces
+            reversed_script_text = script_text[::-1]
+            for i in range(closing_brace_index):
+                closing_brace_index = reversed_script_text.find('}')
+                script_text = script_text[:-closing_brace_index]
+
+            athlete_info = json.loads(script_text)["athlete"]
+
+            # Extract the athlete's information from the JSON data
+            athlete_id = athlete_info["id"]
+            firstname = athlete_info["firstname"]
+            lastname = athlete_info["lastname"]
+            country = athlete_info["country"]
+            birthday = athlete_info["birthday"]
+            gender = athlete_info["gender"]
+            height = athlete_info["height"]
+            speed_personal_best_score = athlete_info["speed_personal_best"]["time"]
+            speed_personal_best_date = athlete_info["speed_personal_best"]["date"]
+            speed_personal_best_round = athlete_info["speed_personal_best"]["round_name"]
+
+            # Create an athlete item and save it to the database
+            yield AthleteItem(
+                athlete_id=athlete_id,
+                firstname=firstname,
+                lastname=lastname,
+                country=country,
+                birthday=birthday,
+                gender=gender,
+                height=height,
+                speed_personal_best_score=speed_personal_best_score,
+                speed_personal_best_date=speed_personal_best_date,
+                speed_personal_best_round=speed_personal_best_round
+            )
+
+    def parse_entry(self, response, category_id, athlete_id, rank, event_date):
         pass
